8eef664ca626eb3416d01d87bc614311eca92499
Jonathan <jonathanwford156@gmail.com>
2020-12-04 17:56:11 -0500

    add @angular/material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TitleCasePipe } from './title-case.pipe';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { TitleCasePipe } from './title-case.pipe';
     TitleCasePipe
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
