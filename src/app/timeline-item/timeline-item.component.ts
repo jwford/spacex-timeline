2f64eaa17bb2d8bfd614718bee264d3c6f4860ed
Jonathan <jonathanwford156@gmail.com>
2020-12-04 20:28:41 -0500

    add launch date to timeline items

--- src/app/timeline-item/timeline-item.component.ts
+++ src/app/timeline-item/timeline-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Launch } from 'src/types/spacex';
 
+import * as moment from 'moment';
+
 @Component({
   selector: 'app-timeline-item',
   templateUrl: './timeline-item.component.html',
@@ -13,6 +15,14 @@ export class TimelineItemComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  formatLaunchDateLocal() {
+    return this.launch?.launch_date_local ? moment(this.launch!.launch_date_local).format('YYYY MMMM Do') : null;
+  }
+
+  formatLaunchDateUTC() {
+    return this.launch?.launch_date_utc ? moment(this.launch!.launch_date_utc).format('YYYY MMMM Do') : null;
   }
 
 }
