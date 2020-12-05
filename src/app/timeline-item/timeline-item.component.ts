88db67f3a3cc66a5c331f9c0e25660aa0580699e
Jonathan <jonathanwford156@gmail.com>
2020-12-05 09:28:59 -0500

    remove ngOnInit where unecessary

--- src/app/timeline-item/timeline-item.component.ts
+++ src/app/timeline-item/timeline-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Launch } from 'src/types/spacex';
 
 import * as moment from 'moment';
@@ -8,14 +8,9 @@ import * as moment from 'moment';
   templateUrl: './timeline-item.component.html',
   styleUrls: ['./timeline-item.component.scss']
 })
-export class TimelineItemComponent implements OnInit {
+export class TimelineItemComponent {
 
   @Input() launch: Launch | null = null;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
 
   formatLaunchDateLocal() {
     return this.launch?.launch_date_local ? moment(this.launch!.launch_date_local).format('YYYY MMMM Do') : null;
