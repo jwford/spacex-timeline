a50f46fd43e2c775ae28041a02bceae636da37be
Jonathan <jonathanwford156@gmail.com>
2020-12-06 08:56:33 -0500

    add toolbar with filter button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
 
 import { MzdTimelineModule } from 'ngx-mzd-timeline';
 
@@ -10,20 +12,24 @@ import { TitleCasePipe } from './title-case.pipe';
 import { LaunchTimelineComponent } from './launch-timeline/launch-timeline.component';
 import { TimelineItemComponent } from './timeline-item/timeline-item.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TitleCasePipe,
     LaunchTimelineComponent,
-    TimelineItemComponent
+    TimelineItemComponent,
+    ToolbarComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     HttpClientModule,
     MzdTimelineModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    MatToolbarModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
